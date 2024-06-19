372480c60f3f2367e5adbf66b0c6a76138e5f250
Alexy <lahaye.alexy1804@gmail.com>
2024-06-19 18:36:41 +0200

    Gestion des utilisateurs (Partie lecture seule)

--- src/views/Admin/AdminUsers.js
+++ src/views/Admin/AdminUsers.js
@@ -1,60 +1,104 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import AnimationRevealPage from "helpers/AnimationRevealPage.js";
-import {loadUsers} from "../../models/UserModel";
+import { loadUsers } from "../../models/UserModel";
 import styled from "styled-components";
 import tw from "twin.macro";
 import { css } from "styled-components/macro"; //eslint-disable-line
 import { ContentWithPaddingXl, Container } from "components/misc/Layouts.js";
-import {SectionHeading as Heading, Subheading as SubheadingBase} from "components/misc/Headings.js";
-import { ReactComponent as SvgDecoratorBlob1 } from "../../assets/images/svg-decorator-blob-7.svg";
-import { ReactComponent as SvgDecoratorBlob2 } from "../../assets/images/svg-decorator-blob-8.svg";
+import { SectionHeading, Subheading as SubheadingBase } from "components/misc/Headings.js";
+import { SectionDescription } from "components/misc/Typography";
+import { ReactComponent as TwitterIcon } from "../../assets/images/twitter-icon.svg";
+import { ReactComponent as LinkedinIcon } from "../../assets/images/linkedin-icon.svg";
+import { ReactComponent as GithubIcon } from "../../assets/images/github-icon.svg";
+import defaultProfilePic from "../../assets/profilePictures/defaultPP.png";
 
-const AdminUser = tw.div`flex flex-col lg:flex-row items-center lg:items-stretch`;
-const TestimonialContainer = tw.div`mt-16 lg:w-1/3`;
-const Testimonial = tw.div`px-4 text-center max-w-xs mx-auto flex flex-col items-center`;
-const Image = tw.img`w-20 h-20 rounded-full`;
-const CustomerName = tw.p`mt-3 text-gray-900 font-semibold uppercase text-sm tracking-wide`;
-const Subheading = tw(SubheadingBase)`mt-5 text-center`;
+const HeadingContainer = tw.div``;
+const Heading = tw(SectionHeading)``;
 
-const DecoratorBlob1 = styled(SvgDecoratorBlob1)`
-  ${tw`pointer-events-none -z-20 absolute left-0 top-0 h-56 w-56 opacity-15 transform -translate-x-2/3 -translate-y-12 text-teal-400`}
+const Users = tw.div`flex flex-wrap flex-row justify-center sm:max-w-2xl lg:max-w-5xl mx-auto`;
+const User = tw.div`mt-24 w-full sm:w-1/2 lg:w-1/3 flex flex-col items-center`;
+const UserImage = styled.div`
+  ${props => css`background-image: url("${props.imageSrc}");`}
+  ${tw`w-64 h-64 bg-contain bg-center rounded`}
 `;
-const DecoratorBlob2 = styled(SvgDecoratorBlob2)`
-  ${tw`pointer-events-none -z-20 absolute right-0 bottom-0 h-64 w-64 opacity-15 transform translate-x-2/3 text-yellow-500`}
+const UserContent = styled.div`
+  ${tw`flex flex-col items-center mt-6`}
+  .position {
+    ${tw`uppercase font-bold tracking-widest text-xs text-primary-500`}
+  }
+  .name {
+    ${tw`mt-1 text-xl font-medium text-gray-900`}
+  }
 `;
 
-export default (
-    {heading = "Administration des utilisateurs", token}
-) =>{
+const UserLinks = styled.div`
+  ${tw`mt-6 flex`}
+  .link {
+    ${tw`mr-8 last:mr-0 text-gray-400 hocus:text-primary-500 transition duration-300`}
+    .icon {
+      ${tw`fill-current w-6 h-6`}
+    }
+  }
+`;
+
+const getProfileImagePath = (id_user) => {
+    try {
+        return require(`../../assets/profilePictures/${id_user}.png`);
+    } catch (error) {
+        return defaultProfilePic;
+    }
+};
+
+const getRoleName = (role) => {
+    switch(role) {
+        case 0:
+            return "Administrateur";
+        case 1:
+            return "Atelier";
+        case 2:
+            return "Commercial";
+        default:
+            return "Inconnu";
+    }
+};
+
+export default ({
+                    heading = "Administration utilisateurs",
+                }) => {
     const [allUsers, setAllUsers] = useState([]);
 
     useEffect(() => {
-            loadUsers().then(data => {
-                setAllUsers(data);
-                console.log(data);
-            });
+        loadUsers().then(data => {
+            setAllUsers(data);
+        });
     }, []);
 
     return (
         <AnimationRevealPage>
             <Container>
                 <ContentWithPaddingXl>
-                    <Heading>{heading}</Heading>
-                    <AdminUser>
+                    <HeadingContainer>
+                        {heading && <Heading>{heading}</Heading>}
+                    </HeadingContainer>
+                    <Users>
                         {allUsers.length > 0 && allUsers.map((user, index) => (
-                            <TestimonialContainer key={index}>
-                                <Testimonial>
-                                    <Image src={user.imageSrc} />
-                                    <Subheading>{user.role_user}</Subheading>
-                                    <CustomerName>{user.nom_user}</CustomerName>
-                                </Testimonial>
-                            </TestimonialContainer>
+                            <User key={index}>
+                                <UserImage imageSrc={getProfileImagePath(user.id_user)} />
+                                <UserContent>
+                                    <span className="position">{getRoleName(user.role_user)}</span>
+                                    <span className="name">{user.nom_user}</span>
+                                    <UserLinks>
+                                        {user.links && user.links.map((link, linkIndex) => (
+                                            <a key={linkIndex} className="link" href={link.url}>
+                                                <link.icon className="icon" />
+                                            </a>
+                                        ))}
+                                    </UserLinks>
+                                </UserContent>
+                            </User>
                         ))}
-                    </AdminUser>
+                    </Users>
                 </ContentWithPaddingXl>
-
-                <DecoratorBlob1 />
-                <DecoratorBlob2 />
             </Container>
         </AnimationRevealPage>
     );
