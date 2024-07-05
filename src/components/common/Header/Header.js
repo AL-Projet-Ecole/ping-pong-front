71e0061b0c1969dc239ce8eb527ec1f1cdfa3444
AlexyLahaye <90752843+AlexyLahaye@users.noreply.github.com>
2024-07-05 08:51:23 +0200

    Last push part 2 Erreur de la partie header

--- src/components/common/Header/Header.js
+++ src/components/common/Header/Header.js
@@ -2,13 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav, Button } from 'react-bootstrap';
 import { logout } from '../../../models/AuthModel';
-import { jwtDecode } from 'jwt-decode'; // Correction de l'import de jwtDecode
+import {jwtDecode} from 'jwt-decode'; // Correction de l'import de jwtDecode
 import theme from '../../../assets/themes/theme';
 
 function Header() {
-    // Récupérer le rôle à partir du token décodé
-    const decodedToken = jwtDecode(sessionStorage.getItem('token'));
-    const role = decodedToken.role_user;
+    let role = null;
+
+    // Vérifier si le token existe
+    const token = sessionStorage.getItem('token');
+    if (token) {
+        const decodedToken = jwtDecode(token);
+        role = decodedToken.role_user;
+    }
 
     // Fonction pour déterminer si un lien doit être affiché en fonction du rôle
     const isLinkVisible = (linkRole) => {
@@ -27,13 +32,19 @@ function Header() {
             <Navbar.Toggle aria-controls="navbarScroll" />
             <Navbar.Collapse id="navbarScroll">
                 <Nav className="me-auto" navbarScroll>
-                    {isLinkVisible(1) && <Nav.Link as={Link} to="/HubAtelier">Atelier</Nav.Link>}
-                    {isLinkVisible(2) && <Nav.Link as={Link} to="/HubCommercial">Commercial</Nav.Link>}
-                    {isLinkVisible(0) && <Nav.Link as={Link} to="/Admin">Admin</Nav.Link>}
+                    {role !== null && isLinkVisible(1) && <Nav.Link as={Link} to="/HubAtelier">Atelier</Nav.Link>}
+                    {role !== null && isLinkVisible(2) && <Nav.Link as={Link} to="/HubCommercial">Commercial</Nav.Link>}
+                    {role !== null && isLinkVisible(0) && <Nav.Link as={Link} to="/Admin">Admin</Nav.Link>}
                 </Nav>
                 <div className="d-flex align-items-center">
-                    <div style={{ width: '10px' }}></div>
-                    <Button onClick={() => logout()}>Déconnexion</Button>
+                    {role !== null ? (
+                        <>
+                            <div style={{ width: '10px' }}></div>
+                            <Button onClick={() => logout()}>Déconnexion</Button>
+                        </>
+                    ) : (
+                        <Nav.Link as={Link} to="/Login">Connexion</Nav.Link>
+                    )}
                 </div>
             </Navbar.Collapse>
         </Navbar>
